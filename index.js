9f98acbf55c3d912fa7a669a9278f733212c3ac6
anna-sahradyan <a.sahradyan72@gmail.com>
2023-04-24 14:35:25 +0400

    changes

--- index.js
+++ index.js
@@ -2,17 +2,31 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
 const dotenv = require("dotenv");
+const path = require("node:path");
 dotenv.config();
 const cors = require("cors");
 const connectDB = require("./config/connectDB");
 
 const taskRouter = require("./routes/task-router.js");
-app.use(cors({
-    origin:["http://localhost:3000","https://to-do-list-app-l57x.onrender.com"]
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use("/api/tasks",taskRouter);
+app.use("/api/tasks", taskRouter);
+//?DEPLOYMENT CODE
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__filename, "../to-do-list-frontend/build")))
+    app.get("*", (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, "../", "to-do-list-frontend", "build", "index.html")
+        )
+    })
+
+} else {
+    app.get("/", (req, res) => {
+    res.send("Home Page")
+    })
+
+}
 
 connectDB();
 app.listen(PORT, () => {
